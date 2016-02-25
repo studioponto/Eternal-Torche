acd35fcb3e315e8be0b7106f1c8017025e1ebf0b
Eurico Sa Fernandes <eurico@ponto.ws>
2016-02-25 09:36:01 +0100

    no message

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '678f9f7005d04c4850ae662f83549920');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c6297552918f574a651513d2adc2e73b');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
